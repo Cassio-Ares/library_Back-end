5fa10d7cbb460c7f171a262a2b422524381b3088
Cassio-Ares <137903978+Cassio-Ares@users.noreply.github.com>
2023-11-15 11:06:44 -0300

    estrutura do Swagger para gerar documentação

---
+++ swagger/autoGenDoc.js
@@ -0,0 +1,38 @@
+const mongooseToSwagger = require('mongoose-to-swagger');
+const swaggerAutogen = require("swagger-autogen")({
+    openapi: "3.0.0",
+    languages: "pt-BR",    
+});
+
+const outputFile = "./swagger_output.json";
+const endPointsFiles = ["../index.js"];
+
+let doc = {
+    info: {
+        version: "1.0.0",
+        title: "API de uma biblioteca",
+        description: "Documentação de uma API de uma biblioteca onde vc vai poder: cadastrar e editar os cadastros de livros ja feitos, encontrar livros ja cadastrado e deletar cadastros de livros."
+        //editar descrição depois 
+    },
+
+    servers: [
+        {
+            url: "http://localhost:4000/",
+            description: "Servidor localhost"
+        },
+        {
+           // url: "http://",
+           // description:"Servidor de produção"
+        }
+    ],
+
+    consumes: ["application/json"],
+    produces: ["application/json"],
+}
+
+swaggerAutogen(outputFile, endPointsFiles, doc).then(()=>{
+    console.log("Documentação do Swagger gerada encontra-se no arquivo em " + outputFile);
+    if(process.env.NODE_ENV !== 'production'){
+        require("../index.js");
+    }
+});
