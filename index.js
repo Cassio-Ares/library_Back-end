5ba925960a8f3ccd66b4052421b1617d255f4b63
Cassio-Ares <137903978+Cassio-Ares@users.noreply.github.com>
2023-11-16 13:28:32 -0300

    bug swagger

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
+/**
+ * importações 
+ */
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const cors = require('cors');  
-const swaggerUi = require("swagger-ui-express");
+const cors = require('cors');  /**para não termos problema de conflito no front */
+const swaggerUi = require('swagger-ui-express');
+const swaggerOption = {customCssUrl: '/swagger-ui.css'}; /**protege para não ter conflito com a vercel em produção*/
 const routes = require('./src/routes');
-const authDocProducao = require('./src/middlewares/autoDoc');
-
-const swaggerOptions = {customCssUrl:'/swagger-ui.css'}
-
-
+const authDocProducao = require('./src/middleware/authDoc');
 const app = express();
-require('dotenv').config();
+require('dotenv').config();  /**arquivo de variavel de ambiente */
 
 
+/**
+ * configuração do express
+ */
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
@@ -22,21 +25,30 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+/**
+ * documentação Swagger
+ */
 if(process.env.NODE_ENV !== 'test'){
-  const swaggerFile = require("./swagger/swagger_output.json");
-  app.get('/', (req, res) => {/*swagger.ignore = true*/ res.redirect('/doc');});
-  app.use('/doc',authDocProducao, swaggerUi.serve, swaggerUi.setup(swaggerFile, swaggerOptions));  
+    const swaggerFile = require('./swagger/swagger_output.json');
+    app.get('/', (req, res)=>{ /*#swagger.ignore = true*/ res.redirect('/doc'); });
+    app.use('/doc',authDocProducao, swaggerUi.serve, swaggerUi.setup(swaggerFile, swaggerOption));
+   // app.use('/api-docs', express.static('node_modules/swagger-ui-dist/', { index: false }), swaggerUi.serve, swaggerUi.setup(swaggerFile));
 }
 
-routes(app);
 
+/**
+ * end-points rotas da API 
+ */
+routes(app)
+
+
+/**
+ * inicialização do servidor
+ * no express não no bin/www
+ */
 if(process.env.NODE_ENV !== 'test'){
     const PORT = process.env.PORT || 4000;
-    app.listen(PORT, () => {
-        console.log(`Servidor rodando na porta ${PORT}`);
-    });
+    app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
 }
-
-
 
 module.exports = app;
